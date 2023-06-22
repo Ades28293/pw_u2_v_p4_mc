c3c5f8a37132db3cd22be8edaeb968e3385793e1
Michael Cen <cenyungamichael@gmail.com>
2023-06-21 19:45:19 -0500

    Apis

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-<!--<Calculadora></Calculadora> --> 
+<!--<Calculadora></Calculadora> 
 <Contador titulo="Estudiante" v-bind:inicio="4+2"></Contador>
-<Contador titulo="Profesor" :inicio="101"> </Contador>
+<Contador titulo="Profesor" :inicio="101"> </Contador>--> 
+
+<CaracolaMagica></CaracolaMagica>
+
 </template>
 
 <script>
-import Contador from "./components/Contador.vue"
+//import Contador from "./components/Contador.vue"
 //import Calculadora from "./components/Calculadora.vue"
+import CaracolaMagica from "./components/CaracolaMagica.vue"
 
 export default {
   name: 'App',
   components: {
     //Calculadora
-    Contador
+   // Contador
+   CaracolaMagica
    
   }
 }
---
+++ src/components/CaracolaMagica.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Caracola Magica</h1>
+    <img src="https://via.placeholder.com/250" alt="No se puede cargar">
+
+    <input v-model="pregunta" type="text" placeholder="Hazme una Preguntame">
+    <p>Recuerda terminar con un enter la pregunta</p>
+
+    <div>
+        <h2>Voy a pasar de año?</h2>
+        <h1>SI, NO</h1>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            pregunta: 'Hola Mundo'
+        }
+    }
+
+}
+</script>
+
+<style></style>
